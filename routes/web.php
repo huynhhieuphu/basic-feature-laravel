5352c008ba9dbe32dcdf974e5a48946ea9474998
phuhh <huynhhieu.phu@gmail.com>
2023-05-10 14:58:56 +0700

    #5 created migrate

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,3 @@ use App\Http\Controllers\StudentController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::prefix('/student')->name('student.')->group(function() {
-    Route::get('/', [StudentController::class, 'index'])->name('index');
-    Route::get('/create', [StudentController::class, 'create'])->name('create');
-    Route::post('/', [StudentController::class, 'store'])->name('store');
-    Route::get('/{student}/edit/', [StudentController::class, 'edit'])->name('edit');
-    Route::match(['put','patch'],'/{student}', [StudentController::class, 'update'])->name('update');
-    Route::get('/{student}', [StudentController::class, 'show'])->name('show');
-    Route::delete('/{student}', [StudentController::class, 'destroy'])->name('destroy');
-});
